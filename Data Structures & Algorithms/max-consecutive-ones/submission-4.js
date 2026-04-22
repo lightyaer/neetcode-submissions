758a44051c966b6dc81f693424ef54381772be1e
Dhananjay Naik <mmnyed@gmail.com>
2026-04-22 19:41:57 +0530

    Add: max-consecutive-ones - submission-4

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-4.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums) {
+        const reduced = nums.reduce(
+            (acc, item) => {
+                if (item === 1) {
+                    acc[0] += 1;
+                    acc[1] = Math.max(acc[0], acc[1]);
+                }
+
+                if (item === 0) {
+                    acc[0] = 0;
+                }
+
+                return acc;
+            },
+            [0, 0],
+        );
+
+        return reduced[1];
+    }
+}
